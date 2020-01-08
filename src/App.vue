e15b88ddb9b7a2f9ac5efa84665c8de6d5eb6846
YuanHe <15220918751@163.com>
2020-01-08 10:48:26 +0800

    axios架构
    axios错误拦截

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" />
+    <router-view />
   </div>
 </template>
 
@@ -13,7 +13,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -3,8 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+/* eslint-disable */
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+// 根据前端的跨域方式做调整,baseURL
+axios.defaults.baseURL = '/api'
+// 超时时间设置
+axios.defaults.timeout = 8000
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  // 后端返回数据要从data拿出
+  let res = response.data
+  // 根据不同不同状态码，进行数据返回。
+  if (res.status === 0) {
+    return res.data
+  } else if (res.status === 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
