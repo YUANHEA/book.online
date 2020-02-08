a352a5db8398c745a20da7eafa1545d9f81579be
YuanHe <15220918751@163.com>
2020-02-08 10:10:04 +0800

    分页器
    elementUI组件引入报没有调用错误原因
    注册、导入名称和组件在结构中引用名称不同
    动态加载组件变量
    [组件名.name]
    Pagination.name相当于el-pagination。[Pagination.name]，相当于“el-pagination”。相当我们用中括号加载一个动态变量。
    第三方组件布局
    绑定动态属性，通过绑定动态布局，改动组件样式。
    在变量定义及事件中，定义属性值及改动属性值
    第三方组件交互
    通过绑定事件，在事件中实现交互
    在事件中改动属性值，获取接口数据
    改动组件样式
    简单就直接在结构上写。这里改动组件内部样式
    这里改写组件在左右方向布局
    改动组件颜色,组件自带样式改动
    直接照着重写一遍
    组件职务范围
    什么值什么时间点是某个组件控制，什么值什么时间点只是传给某个组件要分清楚
    比如分页这些属性量都是传给分页组件，在init时候。在交互事件中改动这些量
    数组数据拼接
    实现所有数组数据不断累加
    this.list.concat(被拼接数组)
    this.list.concat(res.list)
    控制按钮居中
    在按钮结构标签外围加div,再text-aligh:center
    滚动加载数据插件
    infinite-scroll-distance=“距离底部多少加载数据”
    在第一次触发滚动加载后将busy设置为true，先不载进行滚动加载数据，就算此时多次滚动。
    等数据回来再次将滚动加载busy打开
    如何让第一次不触发滚动加载
    关键在于第一次
    在请求方法中，在接口请求先设置busy为true繁忙不加载，等接口数据返回时再设置为false

--- src/components/order/components/orderList.vue
+++ src/components/order/components/orderList.vue
@@ -1,102 +1,148 @@
 <template>
-  <div class="order-list">
-    <order-header title="订单列表">
-      <template v-slot:tip>
-        <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
-      </template>
-    </order-header>
-    <div class="wrapper">
-      <div class="container">
-        <div class="order-box">
-          <loading v-if="loading"></loading>
-          <div class="order" v-for="(order,index) in list" :key="index">
-            <div class="order-title">
-              <div class="item-info fl">
-                {{order.createTime}}
-                <span>|</span>
-                {{order.receiverName}}
-                <span>|</span>
-                订单号：{{order.orderNo}}
-                <span>|</span>
-                {{order.paymentTypeDesc}}
-              </div>
-              <div class="item-money fr">
-                <span>应付金额：</span>
-                <span class="money">{{order.payment}}</span>
-                <span>元</span>
-              </div>
-            </div>
-            <div class="order-content clearfix">
-              <div class="good-box fl">
-                <div class="good-list" v-for="(item,i) in order.orderItemVoList" :key="i">
-                  <div class="good-img">
-                    <img v-lazy="item.productImage" alt="">
-                  </div>
-                  <div class="good-name">
-                    <div class="p-name">{{item.productName}}</div>
-                    <div class="p-money">{{item.totalPrice + 'X' + item.quantity}}元</div>
-                  </div>
+    <div class="order-list">
+        <order-header title="订单列表">
+            <template v-slot:tip>
+                <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
+            </template>
+        </order-header>
+        <div class="wrapper">
+            <div class="container">
+                <div class="order-box">
+                    <loading v-if="loading"></loading>
+                    <div class="order" v-for="(order,index) in list" :key="index">
+                        <div class="order-title">
+                            <div class="item-info fl">
+                                {{order.createTime}}
+                                <span>|</span>
+                                {{order.receiverName}}
+                                <span>|</span>
+                                订单号：{{order.orderNo}}
+                                <span>|</span>
+                                {{order.paymentTypeDesc}}
+                            </div>
+                            <div class="item-money fr">
+                                <span>应付金额：</span>
+                                <span class="money">{{order.payment}}</span>
+                                <span>元</span>
+                            </div>
+                        </div>
+                        <div class="order-content clearfix">
+                            <div class="good-box fl">
+                                <div
+                                    class="good-list"
+                                    v-for="(item,i) in order.orderItemVoList"
+                                    :key="i"
+                                >
+                                    <div class="good-img">
+                                        <img v-lazy="item.productImage" alt />
+                                    </div>
+                                    <div class="good-name">
+                                        <div class="p-name">{{item.productName}}</div>
+                                        <div
+                                            class="p-money"
+                                        >{{item.totalPrice + 'X' + item.quantity}}元</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="good-state fr" v-if="order.status===20">
+                                <a href="javascript:;">{{order.statusDesc}}</a>
+                            </div>
+                            <div class="good-state fr" v-else>
+                                <a
+                                    href="javascript:;"
+                                    @click="goPay(order.orderNo)"
+                                >{{order.statusDesc}}</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="good-state fr" v-if="order.status===20">
-                <a href="javascript:;">{{order.statusDesc}}</a>
-              </div>
-              <div class="good-state fr" v-else>
-                <a href="javascript:;" @click="goPay(order.orderNo)">{{order.statusDesc}}</a>
-              </div>
+                <div class="orderLPnt">
+                  <!-- <el-pagination
+                    class="pagination"
+                    background
+                    layout="prev, pager, next"
+                    @current-change="handleCurrentChange"
+                    :currentPage="pageNum"
+                    :pageSize="pageSize"
+                    :total="total">
+                  </el-pagination> -->
+                </div>
+                <div class="elBtn">
+                    <el-button
+                        type="primary"
+                        :loading="loading"
+                        @click="loadingMore"
+                        v-show="showElBtn"
+                    >加载中</el-button>
+                </div>
+                <div
+                    class="load-more"
+                    v-infinite-scroll="scrollMore"
+                    infinite-scroll-disabled="busy"
+                    infinite-scroll-distance="400"
+                >
+                    <img src="static/imgs/loading-svg/loading-bubbles.svg" alt="#" v-show="showElBtn" />
+                </div>
             </div>
-          </div>
-          <no-data v-if="!loading && list.length===0"></no-data>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 <script>
 import OrderHeader from '@/comment/orderheader/OrderHeader'
 import Loading from '@/comment/loading/Loading'
 import NoData from '@/comment/nodata/NoData'
-
-export default{
+import { Pagination, Button } from 'element-ui'
+import infiniteScroll from 'vue-infinite-scroll'
+export default {
   name: 'order-list',
   components: {
     OrderHeader,
     Loading,
-    NoData
+    NoData,
+    [Pagination.name]: Pagination,
+    [Button.name]: Button
   },
   data () {
     return {
-      loading: false,
-      list: [],
-      pageSize: 10,
-      pageNum: 1,
-      total: 0,
-      showNextPage: true, // 加载更多：是否显示按钮
-      busy: false // 滚动加载，是否触发
+      loading: false, // 显示加载中
+      list: [], // 订单数据列表
+      pageSize: 10, // 每页显示多少数据
+      pageNum: 1, // 当前页
+      total: 0, // 订单数据总数
+      showElBtn: true, // 是否显示加载按钮
+      hasNextPage: true, // 是否有下一页
+      busy: true
     }
   },
+  directives: { infiniteScroll },
   mounted () {
     this.getOrderList()
   },
   methods: {
+    // 获取订单列表
     getOrderList () {
-      this.loading = true
       this.busy = true
-      this.axios.get('/orders', {
-        params: {
-          pageSize: 10,
-          pageNum: this.pageNum
-        }
-      }).then((res) => {
-        this.loading = false
-        this.list = this.list.concat(res.list)
-        this.total = res.total
-        this.showNextPage = res.hasNextPage
-        this.busy = false
-      }).catch(() => {
-        this.loading = false
-      })
+      this.loading = true
+      this.axios
+        .get('/orders', {
+          params: {
+            pageSize: 10,
+            pageNum: this.pageNum
+          }
+        })
+        .then(res => {
+          this.busy = false
+          this.loading = false
+          this.list = this.list.concat(res.list)
+          // this.list = res.list
+          this.total = res.total
+          this.showElBtn = res.hasNextPage
+        })
+        .catch(() => {
+          this.loading = false
+        })
     },
+    // 跳转到支付页面
     goPay (orderNo) {
       // 三种路由跳转方式
       // this.$router.push('/order/pay')
@@ -114,122 +160,144 @@ export default{
         }
       })
     },
-    // 第一种方法：分页器
-    handleChange (pageNum) {
-      this.pageNum = pageNum
+    // 当前页切换
+    handleCurrentChange (val) {
+      this.pageNum = val
       this.getOrderList()
     },
-    // 第二种方法：加载更多按钮
-    loadMore () {
+    // 加载更多订单数据
+    loadingMore () {
+      this.loading = true
       this.pageNum++
       this.getOrderList()
     },
-    // 第三种方法：滚动加载，通过npm插件实现
-    scrollMore () {
+    // 获取订单列表
+    getOrder () {
       this.busy = true
-      setTimeout(() => {
-        this.pageNum++
-        this.getList()
-      }, 500)
-    },
-    // 专门给scrollMore使用
-    getList () {
       this.loading = true
-      this.axios.get('/orders', {
-        params: {
-          pageSize: 10,
-          pageNum: this.pageNum
-        }
-      }).then((res) => {
-        this.list = this.list.concat(res.list)
-        this.loading = false
-        if (res.hasNextPage) {
+      this.axios
+        .get('/orders', {
+          params: {
+            pageSize: 10,
+            pageNum: this.pageNum
+          }
+        })
+        .then(res => {
           this.busy = false
-        } else {
-          this.busy = true
-        }
-      })
+          this.loading = false
+          this.list = this.list.concat(res.list)
+          this.showElBtn = res.hasNextPage
+          if (res.hasNextPage) {
+            this.busy = false
+          } else {
+            this.busy = true
+          }
+        })
+        .catch(() => {
+          this.loading = false
+        })
+    },
+    // 滚动加载更多
+    scrollMore () {
+      // 将busy设置为繁忙，防止在那段刷新距离多次请求
+      this.busy = true
+      if (this.showElBtn) {
+        // 利用定时器减少这种频繁请求
+        setTimeout(() => {
+          this.pageNum++
+          this.getOrder()
+        }, 500)
+      }
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-  @import 'assets/scss/config.scss';
-  @import 'assets/scss/mixin.scss';
-  @import "assets/scss/base.scss";
-  .order-list{
-    .wrapper{
-      background-color:$colorJ;
-      padding-top:33px;
-      padding-bottom:110px;
-      .order-box{
-        .order{
-          @include border();
-          background-color:$colorG;
-          margin-bottom:31px;
-          &:last-child{
-            margin-bottom:0;
-          }
-          .order-title{
-            @include height(74px);
-            background-color:$colorK;
-            padding:0 43px;
-            font-size:16px;
-            color:$colorC;
-            .item-info{
-              span{
-                margin:0 9px;
-              }
-            }
-            .money{
-              font-size:26px;
-              color:$colorB;
-            }
-          }
-          .order-content{
-            padding:0 43px;
-            .good-box{
-              width:88%;
-              .good-list{
-                display: flex;
-                align-items: center;
-                height:145px;
-                .good-img{
-                  width:112px;
-                  img{
-                    width:100%;
-                  }
+@import "assets/scss/config.scss";
+@import "assets/scss/mixin.scss";
+@import "assets/scss/base.scss";
+.order-list {
+    .wrapper {
+        background-color: $colorJ;
+        padding-top: 33px;
+        padding-bottom: 110px;
+        .container {
+            .order-box {
+                .order {
+                    @include border();
+                    background-color: $colorG;
+                    margin-bottom: 31px;
+                    &:last-child {
+                        margin-bottom: 0;
+                    }
+                    .order-title {
+                        @include height(74px);
+                        background-color: $colorK;
+                        padding: 0 43px;
+                        font-size: 16px;
+                        color: $colorC;
+                        .item-info {
+                            span {
+                                margin: 0 9px;
+                            }
+                        }
+                        .money {
+                            font-size: 26px;
+                            color: $colorB;
+                        }
+                    }
+                    .order-content {
+                        padding: 0 43px;
+                        .good-box {
+                            width: 88%;
+                            .good-list {
+                                display: flex;
+                                align-items: center;
+                                height: 145px;
+                                .good-img {
+                                    width: 112px;
+                                    img {
+                                        width: 100%;
+                                    }
+                                }
+                                .good-name {
+                                    font-size: 20px;
+                                    color: $colorB;
+                                }
+                            }
+                        }
+                        .good-state {
+                            @include height(145px);
+                            font-size: 20px;
+                            color: $colorA;
+                            a {
+                                color: $colorA;
+                            }
+                        }
+                    }
                 }
-                .good-name{
-                  font-size:20px;
-                  color:$colorB;
-                }
-              }
             }
-            .good-state{
-              @include height(145px);
-              font-size: 20px;
-              color:$colorA;
-              a{
-                color:$colorA;
-              }
+            .pagination {
+                text-align: right;
             }
-          }
+            .orderLPnt
+                >>> .el-pagination.is-background
+                .el-pager
+                li:not(.disabled).active {
+                background-color: #ff6600;
+            }
+            .elBtn {
+                text-align: center;
+            }
+            .el-button--primary {
+                background-color: #ff6600;
+                border-color: #ff6600;
+            }
+            .load-more,
+            .scroll-more {
+                text-align: center;
+            }
         }
-        .pagination{
-          text-align:right;
-        }
-        .el-pagination.is-background .el-pager li:not(.disabled).active{
-          background-color: #FF6600;
-        }
-        .el-button--primary{
-          background-color: #FF6600;
-          border-color: #FF6600;
-        }
-        .load-more,.scroll-more{
-          text-align:center;
-        }
-      }
     }
-  }
+}
 </style>
